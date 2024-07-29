5218599f23d61d45fadb154337d37968b4cc41bb
BEN <benjamin.guenoun@le-campus-numerique.fr>
2024-07-29 13:53:46 +0200

    Initial Laravel project setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World</title>
+</head>
+<body>
+    <h1>Hello World From Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})</h1>
+</body>
+</html>
